bbd3d73b3201faac03c82456e78331ff29eb1c39
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-12-26 04:47:24 +0530

    Domain Updated as site

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,7 +1,17 @@
+import Navigation from "@/components/site/navigation";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import React from "react";
-import Home from "@/app/site/page";
-const page = () => {
-  return <Home />;
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <main className="h-full">
+        <Navigation />
+        {children}
+      </main>
+    </ClerkProvider>
+  );
 };
 
-export default page;
+export default layout;
