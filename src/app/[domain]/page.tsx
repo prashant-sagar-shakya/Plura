9b063d86e0b3453b7273bfa11f4a7dcab51b30a1
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-12-26 04:58:36 +0530

    domain fixed

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,14 +1,15 @@
 import Navigation from "@/components/site/navigation";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Home from "@/app/site/page";
 import React from "react";
 
-const page = ({ children }: { children: React.ReactNode }) => {
+const page = () => {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <main className="h-full">
         <Navigation />
-        {children}
+        <Home />
       </main>
     </ClerkProvider>
   );
